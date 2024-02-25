75cb403f8741f0b559e04a076f837df9dcc0a3fe
Leonardo Gregório de Almeida <leogreal@gmail.com>
2024-02-24 21:42:03 -0300

    Add nav component

--- cod3r/frontend/src/app/app.module.ts
+++ cod3r/frontend/src/app/app.module.ts
@@ -4,16 +4,25 @@ import {
   provideClientHydration,
 } from '@angular/platform-browser';
 
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { HeaderComponent } from './components/template/header/header.component';
+import { NavComponent } from './components/template/nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule, MatToolbarModule],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
   providers: [provideClientHydration(), provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
