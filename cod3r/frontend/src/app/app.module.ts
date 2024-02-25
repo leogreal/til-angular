12f717422ebd17f11894c2adf0a3947e9e3c3928
Leonardo Gregório de Almeida <leogreal@gmail.com>
2024-02-24 21:01:55 -0300

    Add footer component

--- cod3r/frontend/src/app/app.module.ts
+++ cod3r/frontend/src/app/app.module.ts
@@ -4,15 +4,15 @@ import {
   provideClientHydration,
 } from '@angular/platform-browser';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/template/footer/footer.component';
 import { HeaderComponent } from './components/template/header/header.component';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, MatToolbarModule],
   providers: [provideClientHydration(), provideAnimationsAsync()],
   bootstrap: [AppComponent],
