2598971399a127f6b8c01a12ea660ce2ae948a6c
Leonardo Gregório de Almeida <leogreal@gmail.com>
2024-02-29 19:14:55 -0300

    Add product update

--- cod3r/frontend/src/app/components/product/product.service.ts
+++ cod3r/frontend/src/app/components/product/product.service.ts
@@ -25,7 +25,17 @@ export class ProductService {
     return this.http.post<Product>(this.baseUrl, product);
   }
 
+  update(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.id}`;
+    return this.http.put<Product>(url, product);
+  }
+
   read(): Observable<Product[]> {
     return this.http.get<Product[]>(this.baseUrl);
+  }
+
+  readById(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.get<Product>(url);
   }
 }
